502a5195731c2cbba92562feec5771dc10b857e8
bruno <bruno@192.168.0.8>
2021-07-29 22:25:50 -0300

    Usando números aleatórios em java em uma brincadeira de números da sórte.

---
+++ random/src/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char iniciador;
+		System.out.println("Seja bem vindo ao gerador de números da sórte!\nDigite 's' para gerar o seu! ");
+
+		do {
+			iniciador = sc.next().charAt(0);
+			System.out.println("Informe S para prosseguir.");
+		} while (iniciador != 's');
+
+		System.out.print("O seu número da sórte é ");
+		Random random = new Random();
+		int numero = random.nextInt(100);
+		System.out.println(numero);
+		sc.close();
+	}
+
+}
